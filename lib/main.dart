a9e6c5d977ba6257f42b3cf0a98305f0684a5dff
Rahul Khatri <khatri.rahul019@gmail.com>
2021-05-31 11:16:41 +0530

    category and category List Item UI added

--- lib/main.dart
+++ lib/main.dart
@@ -45,10 +45,27 @@ class FirstHalf extends StatelessWidget {
         SizedBox(height:30),
         searchBar(),
         SizedBox(height:30),
+        categories(),
         ]
       ),
     );
   }
+}
+
+Widget categories(){
+  return Container(height: 185,
+  child: ListView(
+    scrollDirection: Axis.horizontal,
+    children:<Widget>[
+      CategoryListItem(
+        categoryIcon: Icons.bug_report,
+        categoryName: "Burgers",
+        availability: 12,
+        selected: true
+      )
+    ]
+  ),
+  );
 }
 
 Widget searchBar(){
@@ -98,6 +115,74 @@ class CustomAppBar extends StatelessWidget {
       decoration: BoxDecoration(
         color: Colors.yellow[800],
         borderRadius: BorderRadius.circular(50)
+      ),
+    );
+  }
+}
+
+class CategoryListItem extends StatelessWidget {
+
+final IconData categoryIcon;
+final String categoryName;
+final int availability;
+final bool selected;
+
+CategoryListItem(
+  {
+    @required this.categoryIcon,
+    @required this.categoryName,
+    @required this.availability,
+    @required this.selected
+  }
+);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(right:20),
+      padding: EdgeInsets.fromLTRB(10, 10, 10, 20),
+      decoration: BoxDecoration(borderRadius:BorderRadius.circular(5),
+      color: selected ? Color(0xffeb324): Colors.white,
+      border: Border.all(
+        color: selected ? Colors.transparent: Colors.grey[200],
+        width: 1.5
+      ),
+      boxShadow: [
+        BoxShadow(
+          color: Colors.grey[100],
+          blurRadius: 15,
+          offset: Offset(25,0),
+          spreadRadius: 5
+        )
+      ]
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.all(20),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(20),
+              border: Border.all(
+                color: selected ? Colors.transparent : Colors.grey,
+                width: 1.5
+              )
+            ),
+            child: Icon(categoryIcon, color: Colors.black, 
+            size: 30,
+            ),
+            ),
+            SizedBox(height:10),
+            Text(
+              categoryName,
+              style: TextStyle(color: Colors.black,
+              fontWeight: FontWeight.w700),
+            ),
+            Container(
+              margin: EdgeInsets.fromLTRB(0, 0, 0, 10),
+            )
+        ],
       ),
     );
   }
